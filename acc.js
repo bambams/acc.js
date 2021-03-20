d10b71227c1e9b8a49fe2e29c09de25c3efecead
Brandon McCaig <bamccaig@gmail.com>
2021-03-20 01:07:27 -0400

    Additional updates to README; update script copy.

--- acc.js
+++ acc.js
@@ -1,39 +1,47 @@
 /*
- * A custom JS script to enhance http://www.allegro.cc forums.
- * Copyright © 2010, 2012, 2013, 2018, 2021 Brandon McCaig
- *
+ * A custom JS script to enhance https://www.allegro.cc/ forums.
+ * Copyright © 2010, 2012-2013, 2018, 2021 Brandon McCaig
+ * 
  * This script is used to "enhance" the user experience on the forums at
- * http://www.allegro.cc. You may hotlink it[1], copy it, modify it, or
+ * https://www.allegro.cc/. You may hotlink it, copy it, modify it, or
  * distribute it; but I ask that if you do copy or modify it, you leave
- * this copyright notice intact.
- *
- * Original location: https://www.castopulence.org/js/acc.js
- * Minified (lies): https://www.castopulence.org/js/acc.min.js
- *
- * It depends on jQuery and jQuery UI. Be sure to add both
- * to the list of external JavaScript scripts before this script. A
- * convenient way is by using the Google API servers, which Google
- * encourages you to do anyway[2].
- *
- * For example, add the following lines to your external JavaScript
- * script list:
- *
+ * this copyright notice intact and note your changes in a separate file
+ * (e.g., README.forkname).
+ * 
+ *   Hosted version:    https://www.castopulence.org/js/acc.js
+ *   Minfied (maybe):   https://www.castopulence.org/js/acc.min.js
+ *   Bleeding edge:     https://www.castopulence.org/js/acc.dev.js
+ * 
+ * It depends on jQuery and jQuery UI. Be sure to add both to the list of
+ * external JavaScript scripts before this script. A convenient way is by
+ * using the Google API servers, which Google encourages you to do
+ * anyway[1].
+ * 
+ * It also [optionally] depends on jszip, which is used to turn named
+ * <code> blocks into a zip file with the file contents. You only need
+ * jszip if you intend to use this (not too many people name their <code>
+ * tags anyway, but I do :P).
+ * 
+ * For example, add the following lines to your external JavaScript script
+ * list:
+ * 
  * https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js
  * https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js
+ * https://www.castopulence.org/js/jszip.js
  * https://www.castopulence.org/js/acc.min.js
- *
+ * 
+ * You must now invoke bam.install() in order for acc.js to actually be
+ * executed now (perhaps it should be in a bam.accjs namespace..). This is
+ * more or less just the "always run" stuff. You can still invoke
+ * individual methods manually, of course.
+ * 
  * As one might expect, it comes with NO WARRANTY, etc. USE AT OWN RISK.
- * Also note that it may periodically break as I develop live so you
- * might experience issues on http://www.allegro.cc if you're hotlinking
- * (or otherwise using a broken version). ^_^ I am not to be held liable
- * for this.
- *
- * [1] At least until I see how it affects my bandwidth, but I can't
- * imagine it will hurt it any. If you do hotlink, check this notice
- * periodically to see if I've changed my mind or risk me taking
- * advantage. ^_^
- *
- * [2] http://code.google.com/apis/ajaxlibs/documentation/
+ * Also note that it may periodically break as I develop live so you might
+ * experience issues on https://www.allegro.cc/ if you're hotlinking (or
+ * otherwise using a broken version). ^_^ I am not to be held liable for
+ * this. Host your own copy to avoid such breakage.
+ * 
+ * [1] https://code.google.com/apis/ajaxlibs/documentation/
  */
 
 // It appears that jQuery is currently conflicting with A.cc in some way.
